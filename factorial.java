92b7bb0749786c6a9eedc705d8f3d443988183a5
Rangaswamy-07 <rockyrangaswamy@gmail.com>
2025-06-09 14:40:08 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,16 @@
+public class factorial{
+    public int fun(int n){
+        
+        if(n == 1){ 
+            
+            return n;
+     
+         }
+        return n*fun(n-1);
+    }
+    public static void main(String[] args){
+        factorial m = new factorial();
+        
+        System.out.println(m.fun(5));
+    }
+}
